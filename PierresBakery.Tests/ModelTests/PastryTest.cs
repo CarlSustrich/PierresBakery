82fcfe2c75590527b8312e6b26839e095117cbc9
Carl Sustrich <113807749+CarlSustrich@users.noreply.github.com>
2023-02-24 12:18:46 -0800

    pastry done

--- PierresBakery.Tests/ModelTests/PastryTest.cs
+++ PierresBakery.Tests/ModelTests/PastryTest.cs
@@ -4,14 +4,58 @@ using System;
 
 namespace PierresBakery.TestTools{
   
-  public class PastryTests
+  [TestClass]
+  public class PastryTests : IDisposable
   {
-    [TestMethod]
-    public void MethodName_ExpectedFunction_ExpectedOutput()
+    public void Dispose()
     {
-      //Triangle newTriangle = new Triangle(2, 3, 8);
-      //Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
+      Pastry.ClearAll();
     }
+
+    [TestMethod]
+    public void PastryConstructor_CreateInstance_Pastry()
+    {
+      Pastry newPastryOrder = new Pastry(4);
+      Assert.AreEqual(typeof(Pastry), newPastryOrder.GetType());
+    }
+
+    [TestMethod]
+    public void PastryCost_SetCostOfSinglePastry_Int()
+    {
+      int expectedTotal = 2;
+      Pastry newPastryOrder = new Pastry(1);
+      Pastry.PastryCost();
+      Assert.AreEqual(expectedTotal, Pastry.CostTotal);
+    }
+
+    [TestMethod]
+    public void PastryCost_CalculateCostWithoutAnyFreePastries_Int()
+    {
+      int expectedTotal = 6;
+      Pastry newPastryOrder = new Pastry(3);
+      Pastry.PastryCost();
+      Assert.AreEqual(expectedTotal, Pastry.CostTotal);
+    }
+
+    [TestMethod]
+    public void PastryCost_CalculateEveryFourthFree_Int()
+    {
+      int expectedTotal = 6;
+      Pastry newPastryOrder = new Pastry(4);
+      Pastry.PastryCost();
+      Assert.AreEqual(expectedTotal, Pastry.CostTotal);
+    }
+
+    [TestMethod]
+    public void PastryCost_DoesNotCompoundOrderTotals_Int()
+    {
+      int expectedTotal = 2;
+      Pastry newPastryOrder = new Pastry(1);
+      Pastry.PastryCost();
+      Pastry.PastryCost();
+      Assert.AreEqual(expectedTotal, Pastry.CostTotal);
+    }
+    
   }
 
 
